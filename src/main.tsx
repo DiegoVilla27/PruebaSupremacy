dc32b16c8cf9f1076be97ce30923fd6dae02e63a
Diego Villanueva <dv270992@gmail.com>
2023-04-11 15:08:51 +0200

    [config] - Install dependencies and config project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bootstrap'
+import '/public/scss/styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	// <React.StrictMode>
